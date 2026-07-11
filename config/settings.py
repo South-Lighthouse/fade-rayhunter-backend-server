c0ece9dd80259eef6b0bf7ffefd640efa43c649d
Carlos Guerra <cguerrave@gmail.com>
2026-07-11 17:39:05 +0200

    2FA take 10

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django_otp.plugins.otp_totp",
     "django_otp.plugins.otp_static",
     "two_factor",
+    "two_factor.plugins.totp",
     "sensors",
     "ingestion",
     "telemetry",
